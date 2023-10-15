59d2e560d558ff019c34595924cfddc0555f1dfd
yboix <yboix@equipelectronics.com>
2023-10-15 18:20:56 +0200

    Editar libros completado

--- facturacion/urls.py
+++ facturacion/urls.py
@@ -4,5 +4,7 @@ from facturacion.views.libros import *
 
 urlpatterns = [
     path("libros/", render_index_libros, name="lista_libros"),
-    path("libros/crear", crear_libro, name="crear_libro")
+    path("libros/crear", crear_libro, name="crear_libro"),
+    path("libros/editar/<pk>", editar_libro, name="editar_libro"),
+    path("libros/borrar/<pk>", borrar_libro, name="borrar_libro")
 ]
--- facturacion/views/libros.py
+++ facturacion/views/libros.py
@@ -1,11 +1,11 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
-from django.db.models import F, ExpressionWrapper, fields
+from django.db.models import F, ExpressionWrapper, FloatField, Func
 from facturacion.models import *
 
 
 def render_index_libros(request):
-    libros = Libro.objects.annotate(precio_iva=ExpressionWrapper(F('precio') * 1.04, output_field=fields.FloatField()))
+    libros = Libro.objects.annotate(precio_iva=ExpressionWrapper(Func(F('precio') * 1.04, function='ROUND', template='%(function)s(%(expressions)s, 2)'), output_field=FloatField()))
     return render(request, 'libros/index.html', context={'libros': libros})
 
 def crear_libro(request):
@@ -24,5 +24,35 @@ def crear_libro(request):
             nuevo_libro.save()
             messages.add_message(request, messages.SUCCESS, 'Libro añadido correctamente.')
         except Exception as e:
+            print(e)
             messages.add_message(request, messages.ERROR, e)
+    return redirect('/facturacion/libros')
+
+def editar_libro(request, pk):
+    libro = Libro.objects.get(id=pk)
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo_editar', '')
+        cantidad = request.POST.get('cantidad_editar', '')
+        precio = request.POST.get('precio_editar', '')
+        descuento = request.POST.get('descuento_editar', '')
+        try:
+            libro.titulo = titulo
+            libro.cantidad = cantidad
+            libro.precio = precio
+            libro.descuento = descuento
+            libro.save()
+            messages.add_message(request, messages.SUCCESS, 'Libro editado correctamente.')
+        except Exception as e:
+            print(e)
+            messages.add_message(request, messages.ERROR, e)
+    return redirect('/facturacion/libros')
+
+def borrar_libro(request, pk):
+    libro = Libro.objects.get(id=pk)
+    try:
+        libro.delete()
+        messages.add_message(request, messages.SUCCESS, 'Libro borrado correctamente.')
+    except Exception as e:
+        print(e)
+        messages.add_message(request, messages.ERROR, e)
     return redirect('/facturacion/libros')
